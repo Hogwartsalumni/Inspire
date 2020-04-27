2012bdc54d502bf8e27ed197c262703e3691fcd8
EC2 Default User <ec2-user@ip-172-31-47-116.ec2.internal>
2020-04-27 00:36:11 +0000

    added more images and credit to bottom of page

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-#Inspire
+~Inspire~
 
-Hi there! This little website was created for Kode with Klossy event 'Giving Tuesday' on May 5th 2020.
+Hi there! This little website was created for Kode with Klossy (alumni) event 'Giving Tuesday' on May 5th, 2020.
 
 Enjoy!
--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var imagesArray = new Array('img/you-rock.jpg', 'img/you-amazing.png', 'img/stay-home-strong.jpg', 'img/in-this-together.png');
+var imagesArray = new Array('img/you-rock.jpg', 'img/you-amazing.png', 'img/stay-home-strong.jpg', 'img/in-this-together.png', 'img/braver-stronger.jpg', 'img/dont-give-up.jpg', 'img/stay-positive.jpg');
 
 function displayImage() {
     var num = Math.floor(Math.random() * imagesArray.length);
